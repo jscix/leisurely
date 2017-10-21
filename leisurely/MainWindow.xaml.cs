91af986e250f71dafce2190329c0ee9947e2f20e
jesse <jscix1@gmail.com>
2017-10-21 19:55:51 -0400

    Updated Interface Elements

--- leisurely/MainWindow.xaml.cs
+++ leisurely/MainWindow.xaml.cs
@@ -20,9 +20,59 @@ namespace leisurely
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Default UI Element Values
+        /// </summary>
+        private Brush DefaultBackground;
+        private Brush DefaultForeGround;
+
         public MainWindow()
         {
             InitializeComponent();
+            // replace these with static colors
+            DefaultBackground =  minimize.Background;
+            DefaultForeGround = minimize.Foreground;
+        }
+
+        /// <summary>
+        /// Custom Windows dragbar code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        /// <summary>
+        /// Changes Interface Label Elements colors on mouseover
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Label_Hover(object sender, MouseEventArgs e)
+        {
+            
+            Label sendingLabel = (Label)sender;
+
+            sendingLabel.Background = Brushes.Red;
+            sendingLabel.Foreground = Brushes.White;
+        }
+
+        /// <summary>
+        /// Changes Interface Labels colors back to default
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Label_RemoveHover(object sender, MouseEventArgs e)
+        {
+            Label sendingLabel = (Label)sender;
+            sendingLabel.Background = DefaultBackground;
+            sendingLabel.Foreground = DefaultForeGround;
+        }
+
+        private void CloseApplication(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
